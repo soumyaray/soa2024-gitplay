7098ef6e377dd83dfaaa52c0877834696cd2c1a7
Soumya Ray <soumya.ray@gmail.com>
2024-09-25 20:35:08 +0800

    feat: country checkboxes on home page

--- server.rb
+++ server.rb
@@ -9,5 +9,16 @@ class App < Roda
     r.root do
       view('home')
     end
+
+    # post request from home.html
+    r.post 'home' do
+      country = r.params['country']
+      country_info = Api.new.get_country_info(country)
+      if country_info.nil?
+        view('not_found')
+      else
+        view('country_info', locals: { country_info: country_info })
+      end
+    end
   end
 end
